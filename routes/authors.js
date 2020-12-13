4027ed25c926234b3cc667290e73b256e325146b
Roman Guliaev <romanguljaev@gmail.com>
2020-12-13 20:31:33 +0100

    second changes

---
+++ routes/authors.js
@@ -0,0 +1,53 @@
+const express = require ('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+//all authirs
+router.get('/',async (req,res)=> {
+    let searchOptions = {}
+    if(req.query.name !=null && req.query.name !='' ){
+        searchOptions.name = new RegExp(req.query.name,'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index',{
+            authors: authors,
+            searchOptions: req.query})
+    } catch{
+      res.redirect('/')  
+    }
+    
+}) 
+
+//new author route
+router.get('/new', (req,res)=> {
+    res.render('authors/new', {author: new Author()})
+})
+//
+//create author route
+router.post('/',async (req,res)=> {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        res.redirect('authors')
+    } catch {
+        res.render('authors/new',{
+            author: author,
+            errorMessage:'Error creatin author'
+        })
+    }
+    //author.save((err,newAuthor)=>{
+    //     if (err) {
+    //         res.render('authors/new',{
+    //         author: author,
+    //         errorMessage:'Error creatin author'
+    //         }) 
+    //     }   else {
+    //         res.redirect('authors')
+    //     }
+    // })
+}) 
+
+module.exports = router
